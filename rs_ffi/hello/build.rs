c53073c34ebd98bb068eb20ef368216b0807fee8
lelelesdx <sodomyxxx@gmail.com>
2026-05-01 13:39:25 +0800

    that was annoying

--- rs_ffi/hello/build.rs
+++ rs_ffi/hello/build.rs
@@ -8,6 +8,6 @@ const TARGET_DIR: &str = "../target/debug";
 fn main()
 {
 	//let lib_dir = Path::new(LIB_DIR);
-	//println!("cargo:rustc-link-search={TARGET_DIR}");
+	println!("cargo:rustc-link-search={TARGET_DIR}");
 	println!("cargo:rustc-link-lib={LIB_NAME}");
 }
