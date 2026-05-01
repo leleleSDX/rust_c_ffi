819c914cabd67d60fe2e1143f8219580c8f4f7f6
lelelesdx <sodomyxxx@gmail.com>
2026-05-01 18:20:03 +0800

    this is mentally exhcausting

--- rs_ffi/hello/build.rs
+++ rs_ffi/hello/build.rs
@@ -1,13 +1,40 @@
-use std::path::{
-	Path, PathBuf,
+use std::{
+	boxed::{
+		Box,
+	},
+	env,
+	error::{
+		Error as StdE,
+	},
+	result::{
+		Result as StdR,
+	},
+	path::{
+		Path, PathBuf,
+	},
 };
-const LIB_DIR: &str = "../../c_lib/hello";
-const LIB_FILE: &str = "libhello.so";
+type SR<T> = StdR<T, Box<dyn StdE>>;
+type SR_ = SR<()>;
+const OK_: SR_ = Ok(());
+const LIB_DIR: &str = "";
+//const LIB_FILE: &str = "libhello.so";
 const LIB_NAME: &str = "hello";
-const TARGET_DIR: &str = "../target/debug";
-fn main()
+fn main()-> SR_
 {
-	//let lib_dir = Path::new(LIB_DIR);
-	println!("cargo:rustc-link-search={TARGET_DIR}");
-	println!("cargo:rustc-link-lib={LIB_NAME}");
+	let crate_root 
+	= env::var("CARGO_MANIFEST_DIR")?;
+	let lib_dir 
+	= Path::new(&crate_root).join(LIB_DIR);
+	if lib_dir.try_exists()? == false
+	{
+		return Err("lib dir not found".into());
+	}
+	println!(
+		"cargo:rustc-link-search=native={}",
+		lib_dir.display()
+	);
+	println!(
+		"cargo:rustc-link-lib=static={LIB_NAME}"
+	);
+	OK_
 }
