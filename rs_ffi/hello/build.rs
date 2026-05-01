f50ca41e81abc1dc27dfe575cd498e734a0e88ec
lelelesdx <sodomyxxx@gmail.com>
2026-05-01 13:24:32 +0800

    custom building

---
+++ rs_ffi/hello/build.rs
@@ -0,0 +1,13 @@
+use std::path::{
+	Path, PathBuf,
+};
+const LIB_DIR: &str = "../../c_lib/hello";
+const LIB_FILE: &str = "libhello.so";
+const LIB_NAME: &str = "hello";
+const TARGET_DIR: &str = "../target/debug";
+fn main()
+{
+	//let lib_dir = Path::new(LIB_DIR);
+	//println!("cargo:rustc-link-search={TARGET_DIR}");
+	println!("cargo:rustc-link-lib={LIB_NAME}");
+}
