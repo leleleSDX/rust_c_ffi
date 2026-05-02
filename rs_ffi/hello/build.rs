ea7083a323abe034cd38150444c19bcb029da263
lelelesdx <sodomyxxx@gmail.com>
2026-05-02 10:37:15 +0800

    cleaning

--- rs_ffi/hello/build.rs
+++ rs_ffi/hello/build.rs
@@ -16,21 +16,16 @@ use std::{
 type SR<T> = StdR<T, Box<dyn StdE>>;
 type SR_ = SR<()>;
 const OK_: SR_ = Ok(());
-const LIB_SRC: &str = "../../c_lib/hello/src";
-const LIB_INC: &str = "../../c_lib/hello/include";
-const LIB_DIR: &str = "../../c_lib/target";
+const C_LIB: &str = "../../c_lib";
+const LIB_SRC: &str = "src";
+const LIB_INC: &str = "include";
+const LIB_DIR: &str = "target";
 //const LIB_FILE: &str = "libhello.so";
 const LIB_NAME: &str = "hello";
 fn main()-> SR_
 {
-	//let crate_root 
-	//= env::var("CARGO_MANIFEST_DIR")?;
 	let lib_dir 
-	= Path::new(LIB_DIR).canonicalize()?;
-	if lib_dir.try_exists()? == false
-	{
-		return Err("lib dir not found".into());
-	}
+	= Path::new(C_LIB).join(LIB_DIR).canonicalize()?;
 	println!(
 		"cargo:rustc-link-search=native={}",
 		lib_dir.display()
@@ -39,13 +34,13 @@ fn main()-> SR_
 		"cargo:rustc-link-lib=static={LIB_NAME}"
 	);
 	let lib_src
-	= Path::new(LIB_SRC).canonicalize()?;
+	= Path::new(C_LIB).join(LIB_NAME).join(LIB_SRC).canonicalize()?;
 	println!(
 		"cargo::rerun-if-changed={}",
 		lib_src.display()
 	);
 	let lib_inc
-	= Path::new(LIB_INC).canonicalize()?;
+	= Path::new(C_LIB).join(LIB_NAME).join(LIB_INC).canonicalize()?;
 	println!(
 		"cargo::rerun-if-changed={}",
 		lib_inc.display()
